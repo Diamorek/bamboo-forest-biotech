b7bb3a2a0a84336074a45cb073e28699d1398b05
Diamorek <96295186+Diamorek@users.noreply.github.com>
2026-09-22 00:04:33 +0900

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const authRoutes = require('./routes/auth')
-  app.use('/api/auth', authRoutes);
-
 // Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173'
